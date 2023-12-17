bddc567204f4b21d5710f1e1b212dbcbf803b0d1
JDL001J <116929245+JDL001J@users.noreply.github.com>
2023-12-17 16:52:29 -0600

    basic calculator set up

---
+++ script.js
@@ -0,0 +1,15 @@
+screen = document.getElementById('text-box');
+
+numObject = {
+     firstNum: 
+     secondNum:
+     operation:
+}
+
+
+
+function operate(){
+    screen.value = "ehofsdkfa"
+}
+
+operate()
